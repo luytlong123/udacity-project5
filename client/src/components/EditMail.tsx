08eed90a3965a166f1608c18bb6009451f0e9963
luytlong123 <101714024+luytlong123@users.noreply.github.com>
2022-12-16 22:45:08 +0700

    Update EditMail.tsx

--- client/src/components/EditMail.tsx
+++ client/src/components/EditMail.tsx
@@ -26,7 +26,7 @@ export class EditMailItem extends React.PureComponent<
   mailItem: {
       content: "",
       mailDestination: "",
-      sendDate: format(new Date().setHours(new Date().getHours()+1), 'yyyy/MM/dd HH:mm:ss'),
+      sendDate: format(new Date().setHours(new Date().getHours()+1), "yyyy-MM-dd'T'HH:mm:ss"),
       sendWithAttachment: false,
       title: "",
       itemId: "",
@@ -119,7 +119,7 @@ export class EditMailItem extends React.PureComponent<
           </Form.Field>
           <Form.Field>
             <label>Date and Time expected</label>
-            <input placeholder='yyyy-MM-ddThh:mm:ss' defaultValue={format(new Date().setDate(new Date().getDate()+1), 'yyyy/MM/dd HH:mm:ss')} value={this.state.mailItem.sendDate}
+            <input placeholder='yyyy-MM-ddThh:mm:ss' defaultValue={format(new Date().setHours(new Date().getHours()+1), "yyyy-MM-dd'T'HH:mm:ss")} value={this.state.mailItem.sendDate}
               disabled />
           </Form.Field>
           <Form.Field>
